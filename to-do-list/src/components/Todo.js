fbf4e343656b4092172ff84dcab0f05bb68f5435
fedalaamira <ga_fedala@esi.dz>
2021-01-09 22:31:52 +0100

    skaalab app

---
+++ to-do-list/src/components/Todo.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+import { Container, Draggable } from 'react-smooth-dnd';
+import {DragDropContext} from 'react-beautiful-dnd'
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo },props) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return(
+  <div>
+      <Container >
+  {todos.map((todo, index) => (
+    <Draggable key={index}>
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+        />
+      </div>
+      </div> 
+      </Draggable>
+  ) )
+  }
+  </Container>
+  </div>
+ );
+};
+
+export default Todo;
